a165a1cbc21cc806aaaa41a161d56e5022997962
gestionece <63755239+gestionece@users.noreply.github.com>
2021-02-09 10:03:22 +0100

    Update Print GeCo.user.js

--- Print GeCo.user.js
+++ Print GeCo.user.js
@@ -37,27 +37,22 @@
         a.document.write('<style>body{opacity: 0}@media print { body{opacity: 1}}</style>');
         a.document.write('</head><body style="overflow: hidden;">');
         a.document.write(resultList);
-        a.document.write("<script>");
+       a.document.write("<script>");
         a.document.write('document.querySelector("#result_list > thead > tr > th.action-checkbox-column").style.opacity = "0";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-stato_esecuzione_display_html").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-tipo_misuratore").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-collocazione_contatore").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-orario_assegnazione").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-lotto").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-contratto").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-data_esecuzione_display").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-data_sopralluogo_display").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-data_sopralluogo_display").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.column-contatore_link").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-codice_ante_sostituzione").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-matricola_ante_sostituzione").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-pod").style.display = "none";');
         a.document.write('document.querySelector("#result_list > thead > tr > th.sortable.column-idrda").style.display = "none";');
+
         a.document.write('var operators = document.querySelector("#result_list > tbody").childElementCount;');
         a.document.write('var i;');
         a.document.write('for (i = 1; i <= operators; i++) {');
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-stato_esecuzione_display_html").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-tipo_misuratore").style.display = "none";');
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-collocazione_contatore").style.display = "none";');
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-orario_assegnazione").style.display = "none";');
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-lotto.nowrap").style.display = "none";');
@@ -65,10 +60,8 @@
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-data_esecuzione_display").style.display = "none";');
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-data_sopralluogo_display").style.display = "none";');
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-contatore_link").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-codice_ante_sostituzione").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-matricola_ante_sostituzione").style.display = "none";');
-        a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-pod").style.display = "none";');
         a.document.write('document.querySelector("#result_list > tbody > tr:nth-child(" + i + ") > td.field-idrda").style.display = "none";');
+
         a.document.write('}');
         a.document.write("</script>");
         a.document.write("</body></html>");
