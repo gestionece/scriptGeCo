7c86ce631c9c8e9236aef0517e9cec62fd5099a2
gestionece <63755239+gestionece@users.noreply.github.com>
2021-03-22 12:15:14 +0100

    Update Position CE.user.js

--- Position CE.user.js
+++ Position CE.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Position CE
 // @namespace    https://github.com/gestionece/scriptGeCo
-// @version      0.4
+// @version      0.5
 // @description  Posizione del contatore
 // @author       Ruslan Dzyuba(Trorker)
 // @match        https://geco.impresalevratti.it/admin/backend/pratica/*/change/*
@@ -12,8 +12,8 @@
     'use strict';
 
     // Your code here...
-    var lat = document.querySelector("#pratica_form > div > fieldset:nth-child(6) > div.form-row.field-latitudine.field-longitudine > div.fieldBox.field-latitudine > div");
-    var lng = document.querySelector("#pratica_form > div > fieldset:nth-child(6) > div.form-row.field-latitudine.field-longitudine > div.fieldBox.field-longitudine > div");
+    var lat = document.querySelector("#fieldsets-container > fieldset:nth-child(6) > div.form-row.field-latitudine.field-longitudine > div.fieldBox.field-latitudine > div");
+    var lng = document.querySelector("#fieldsets-container > fieldset:nth-child(6) > div.form-row.field-latitudine.field-longitudine > div.fieldBox.field-longitudine > div");
 
     var btn = document.createElement("li");
     btn.innerHTML = '<a href="http://www.google.com/maps/place/' + lat.textContent.replace(",", ".") + ',' + lng.textContent.replace(",", ".") + '" class="historylink" target="_blank">Posizione</a>';
